9b09f33daf0bc9a74de2799e79d2bb7f39a67e93
Thomas Garayua <tgarayua@gmail.com>
2023-11-12 23:45:59 -0500

    Created MVVM project layout

---
+++ MealFetcher/Support/MealFetcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealFetcherApp.swift
+//  MealFetcher
+//
+//  Created by Thomas Garayua on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MealFetcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
