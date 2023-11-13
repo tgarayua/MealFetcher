d32e22619b250579f96341beb60109cb3a2ced5a
Thomas Garayua <tgarayua@gmail.com>
2023-11-13 00:03:24 -0500

    Created the Meal model and the networkManager file

---
+++ MealFetcher/Model/Meal.swift
@@ -0,0 +1,14 @@
+//
+//  Meal.swift
+//  MealFetcher
+//
+//  Created by Thomas Garayua on 11/12/23.
+//
+
+import Foundation
+
+struct Meal: Codable {
+    let strMeal: String
+    let strMealThumb: String
+    let idMeal: String
+}
---
+++ MealFetcher/Network/NetworkManager.swift
@@ -0,0 +1,42 @@
+//
+//  NetworkManager.swift
+//  MealFetcher
+//
+//  Created by Thomas Garayua on 11/12/23.
+//
+
+import Foundation
+
+class NetworkManager {
+    private let mealsUrl = "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert"
+    static let shared = NetworkManager()
+    private init() {}
+    
+    func fetchMeals(completion: @escaping ([Meal]?) -> Void) {
+        guard let url = URL(string: mealsUrl) else {
+            print("Error: Unable to find meals API url.")
+            completion(nil)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                print("Error fetching meals with URLSession: ", error.localizedDescription)
+                completion(nil)
+                return
+            }
+            
+            if let data = data {
+                do {
+                    let meals = try JSONDecoder().decode([Meal].self, from: data)
+                    completion(meals)
+                } catch {
+                    print("Error decoding meals data: ", error.localizedDescription)
+                    completion(nil)
+                }
+            } else {
+                completion(nil)
+            }
+        }.resume()
+    }
+}
